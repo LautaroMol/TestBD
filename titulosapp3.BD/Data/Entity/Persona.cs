89491eefc655a3114b84b1a5d06439a4df64957e
Lautaro <laumol159@gmail.com>
2023-05-24 17:52:32 -0300

    first commit

---
+++ titulosapp3.BD/Data/Entity/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace titulosapp3.BD.Data.Entity
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Dni { get; set; } = "";
+        public string Nombre { get; set; } = "";
+        public string Apellido { get; set; } = "";
+    }
+}
